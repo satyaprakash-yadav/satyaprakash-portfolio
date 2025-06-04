5aa2c9178aeaac749f5692fc54c98eadfef975a4
satyaprakash-yadav <satyay9798@gmail.com>
2025-06-04 23:49:37 +0530

    feat: Create about section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import { About } from "@/components/landing/about";
 import { Header } from "@/components/landing/header";
 import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="container mx-auto relative">
       <div className="absolute top-5 right-5">
         <ModeToggle />
       </div>
       <Header />
+      <About />
     </main>
   );
 }
