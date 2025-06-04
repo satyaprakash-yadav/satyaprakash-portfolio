81c4758c209259a30dcc1b5765a63bf2e76e014b
satyaprakash-yadav <satyay9798@gmail.com>
2025-06-05 00:22:44 +0530

    feat: Create experience section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { About } from "@/components/landing/about";
+import { Experience } from "@/components/landing/experience";
 import { Header } from "@/components/landing/header";
 import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <main className="container mx-auto relative">
+    <main className="container px-4 md:px-8 mx-auto relative">
       <div className="absolute top-5 right-5">
         <ModeToggle />
       </div>
       <Header />
       <About />
+      <Experience />
     </main>
   );
 }
