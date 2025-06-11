80a29ac620d086be91f474729d8be2a9f8e0e5dc
satyaprakash-yadav <satyay9798@gmail.com>
2025-06-11 12:13:23 +0530

    feat: Add session provider Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
+import { getServerSession } from "next-auth";
+
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/providers/theme-provider";
-
+import SessionProvider from "@/providers/session-provider";
 
 const poppins = Poppins({
   style: ["normal"],
@@ -18,23 +20,27 @@ export const metadata: Metadata = {
     "Hi! I am Satyaprakash and this is my personal website. Take a look at some of my projects at the portfolio section and do contact me.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession();
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${poppins.className} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster />
-          {children}
-        </ThemeProvider>
+        <SessionProvider session={session}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster />
+            {children}
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
