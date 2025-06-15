c8afbad2efc9a7675b7a1c68df3376604074c303
satyaprakash-yadav <satyay9798@gmail.com>
2025-06-16 03:17:34 +0530

    feat: Create data table Added

---
+++ src/app/(secured)/admin/(routes)/qualification/page.tsx
@@ -0,0 +1,59 @@
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+import { prismadb } from "@/lib/prismadb";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
+
+import { columns } from "@/modules/qualifications/ui/components/columns";
+import { DataTable } from "@/components/data-table/data-table";
+
+export const options = [
+    {
+        label: "Education",
+        value: "EDUCATION",
+    },
+    {
+        label: "Experience",
+        value: "EXPERIENCE"
+    }
+]
+
+const QualificationPage = async () => {
+    const session = await auth();
+
+    if (!session || !session.user) {
+        redirect("/sign-in");
+    };
+
+    const qualifications = await prismadb.qualification.findMany({
+        where: {
+            userId: session.user.id!,
+        },
+        orderBy: {
+            createdAt: "asc",
+        }
+    });
+
+    return (
+        <Card className="rounded-lg border-none">
+            <CardHeader className="mx-[1px] pb-9">
+                <CardTitle className="text-xl font-bold">Qualification</CardTitle>
+                <CardDescription>
+                    Manage your qualification section information.
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <DataTable data={qualifications} columns={columns} options={options} />
+            </CardContent>
+        </Card>
+    );
+}
+
+export default QualificationPage;
---
+++ src/components/data-table/data-table-view-options.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { Table } from "@tanstack/react-table";
+import { MixerHorizontalIcon } from "@radix-ui/react-icons";
+
+import { Button } from "../ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+
+interface DataTableViewOptionsProps<TData> {
+    table: Table<TData>;
+};
+
+export function DataTableViewOptions<TData>({
+    table
+}: DataTableViewOptionsProps<TData>) {
+    return (
+        <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    className="ml-auto hidden h-8 lg:flex"
+                >
+                    <MixerHorizontalIcon className="mr-2 size-4" />
+                    View
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[150px]">
+                <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {table
+                    .getAllColumns()
+                    .filter(
+                        (column) =>
+                            typeof column.accessorFn !== "undefined" && column.getCanHide()
+                    )
+                    .map((column) => {
+                        let columnName = column.id;
+                        if (column.id === "createdAt") {
+                            columnName = "Created";
+                        } else if (column.id === "startYear") {
+                            columnName = "Start";
+                        } else if (column.id === "endYear") {
+                            columnName = "End";
+                        }
+
+                        return (
+                            <DropdownMenuCheckboxItem
+                                key={column.id}
+                                className="capitalize"
+                                checked={column.getIsVisible()}
+                                onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                            >
+                                {columnName}
+                            </DropdownMenuCheckboxItem>
+                        )
+                    })
+                }
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
