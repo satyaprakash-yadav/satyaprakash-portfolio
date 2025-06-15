c4a62b57612d26c3f4b7c06376f88f41773f2a5c
satyaprakash-yadav <satyay9798@gmail.com>
2025-06-15 23:34:52 +0530

    feat: Add mobile sidebar menu

--- src/modules/navbar/ui/components/navbar.tsx
+++ src/modules/navbar/ui/components/navbar.tsx
@@ -1,12 +1,15 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { PageTitle } from "./page-title";
+
 import { UserNav } from "./user-nav";
+import { PageTitle } from "./page-title";
+import { SheetMenu } from "./sheet-menu";
 
 export const Navbar = () => {
   return (
     <header className="z-10 supports-[backdrop-blur-sm]:bg-background/60 sticky top-0 w-full shadow dark:shadow-secondary bg-background/95 backdrop-blur-sm">
-      <div className="mx-8 flex h-14 items-center">
+      <div className="mx-4 sm:mx-8 flex h-14 items-center">
         <div className="flex items-center space-x-4 lg:space-x-0">
+          <SheetMenu />
           <PageTitle />
         </div>
         <div className="flex flex-1 items-center space-x-2 justify-end">
---
+++ src/modules/navbar/ui/components/sheet-menu.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { LayoutTemplate, MenuIcon } from "lucide-react";
+import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "@/modules/dashboard/ui/components/menu";
+
+export const SheetMenu = () => {
+    return (
+        <Sheet>
+            <SheetTrigger className="lg:hidden" asChild>
+                <Button className="h-8" variant="outline" size="icon">
+                    <MenuIcon size={20} />
+                </Button>
+            </SheetTrigger>
+            <SheetTitle className="hidden"></SheetTitle>
+            <SheetContent className="sm:w-72 px-3" side="left" aria-describedby="">
+                <SheetHeader>
+                    <Button
+                        className="flex justify-center items-center pb-2 pt-1"
+                        variant="link"
+                        asChild
+                    >
+                        <Link href="/admin" className="flex items-center gap-2">
+                            <LayoutTemplate />
+                            <h1 className="font-bold text-lg">Portfolio</h1>
+                        </Link>
+                    </Button>
+                </SheetHeader>
+                <Menu isOpen />
+            </SheetContent>
+        </Sheet>
+    )
+}
