845a633ce42deffd17c05d1ad384fc46685a8312
satyaprakash-yadav <satyay9798@gmail.com>
2025-06-18 03:48:42 +0530

    fix: Update file bucket Added

--- src/app/api/edgestore/[...edgestore]/route.ts
+++ src/app/api/edgestore/[...edgestore]/route.ts
@@ -7,13 +7,13 @@ const es = initEdgeStore.create();
  * This is the main router for the Edge Store buckets.
  */
 const edgeStoreRouter = es.router({
-    publicFiles: es.fileBucket().beforeDelete(() => {
+  publicImages: es.imageBucket().beforeDelete(() => {
     return true;
-  })
+  }),
 });
 
 const handler = createEdgeStoreNextHandler({
-    router: edgeStoreRouter,
+  router: edgeStoreRouter,
 });
 
 export { handler as GET, handler as POST };
@@ -21,4 +21,4 @@ export { handler as GET, handler as POST };
 /**
  * This type is used to create the type-safe client for the frontend.
  */
-export type EdgeStoreRouter = typeof edgeStoreRouter; 
+export type EdgeStoreRouter = typeof edgeStoreRouter;
