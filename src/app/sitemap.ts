f344c1bbb9f7977f7aaaa1d33841f44eefaa73a1
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-07-01 00:55:41 +0530

    fix: Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,27 @@
 import { MetadataRoute } from "next";
+import { prismadb } from "@/lib/prismadb";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = process.env.APP_URL
         ? `${process.env.APP_URL}`
         : process.env.VERCEL_URL
             ? `https://${process.env.VERCEL_URL}`
             : `http://localhost:${process.env.PORT || 3000}`;
 
-    return [
+    const portfolios = await prismadb.portfolio.findMany({
+        orderBy: {
+            createdAt: "desc",
+        }
+    });
+
+    const portfoliosMap: MetadataRoute.Sitemap = portfolios.map((portfolio) => ({
+        url: portfolio.demoUrl,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5
+    }));
+
+    const defaultMap: MetadataRoute.Sitemap = [
         {
             url: baseUrl,
             lastModified: new Date(),
@@ -26,5 +40,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: "never",
             priority: 0.8,
         },
-    ]
+    ];
+
+    return [...defaultMap, ...portfoliosMap];
 }
