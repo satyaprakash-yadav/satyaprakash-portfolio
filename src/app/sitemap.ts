29336c34f6e165319546f98086dd9b9b4d09a2df
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-07-12 23:54:40 +0530

    fix: Update robot & sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -42,6 +42,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: "never",
             priority: 0.8,
         },
+        {
+            url: `${baseUrl}/privacy-policy`,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.8
+        },
+        {
+            url: `${baseUrl}/terms-and-conditions`,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.8
+        }
     ];
 
     return [...defaultMap, ...portfoliosMap];
