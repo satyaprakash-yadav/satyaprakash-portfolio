07f349e61fe2db0af32856ac1cc5f55830ccc6a2
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-07-14 00:44:01 +0530

    fix: Update meta title

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign In — Secure Access to Your Account",
+  title: "Sign In — Satyaprakash",
   description:
     "Sign in to your account securely. Access personalized features, update your information, and explore a tailored digital experience. Your gateway to a seamless online presence.",
   alternates: {
@@ -11,13 +11,13 @@ export const metadata: Metadata = {
   },
   openGraph: {
     url: "/sign-in",
-    title: "Sign In — Secure Access to Your Account",
+    title: "Sign In — Satyaprakash",
     description:
       "Sign in to your account securely. Access personalized features, update your information, and explore a tailored digital experience. Your gateway to a seamless online presence."
   },
   twitter: {
     card: "summary_large_image",
-    title: "Sign In — Secure Access to Your Account",
+    title: "Sign In — Satyaprakash",
     description:
       "Sign in to your account securely. Access personalized features, update your information, and explore a tailored digital experience. Your gateway to a seamless online presence."
   }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,7 +3,7 @@ import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign Up — Start Your Digital Journey Today",
+  title: "Sign Up — Satyaprakash",
   description:
     "Create your account and embark on a personalized digital experience. Sign up now to unlock exclusive features, share your story, and connect with a world of opportunities.",
   alternates: {
@@ -11,13 +11,13 @@ export const metadata: Metadata = {
   },
   openGraph: {
     url: "/sign-up",
-    title: "Sign Up — Start Your Digital Journey Today",
+    title: "Sign Up — Satyaprakash",
     description:
       "Create your account and embark on a personalized digital experience. Sign up now to unlock exclusive features, share your story, and connect with a world of opportunities."
   },
   twitter: {
     card: "summary_large_image",
-    title: "Sign Up — Start Your Digital Journey Today",
+    title: "Sign Up — Satyaprakash",
     description:
       "Create your account and embark on a personalized digital experience. Sign up now to unlock exclusive features, share your story, and connect with a world of opportunities."
   }
