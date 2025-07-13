589d726872b775816d1bd4eb56d1b9c1d1b852d8
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-07-14 00:34:34 +0530

    feat: Add schema markup

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,8 @@
 // Server action getData
 import getData from "@/actions/get-data";
 
+import { Person, WithContext } from "schema-dts";
+
 import { Tool } from "@/components/landing/tool";
 import { About } from "@/components/landing/about";
 import { Navbar } from "@/components/landing/navbar";
@@ -29,8 +31,42 @@ export default async function Home() {
     tool,
   } = await getData();
 
+  // create JSON-LD
+  const jsonLd: WithContext<Person> = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Satyaprakash",
+    alternateName: "SARKS",
+    url: "https://www.satyaprakash.com",
+    image: "https://www.satyaprakash.com/satyaprakash.png",
+    jobTitle: miscellaneous
+      ? miscellaneous.titles.map((title) => title.name)
+      : ["Web Developer", "Frontend Developer"],
+    gender: "Male",
+    workLocation: "India",
+    description: "Satyaprakash is an enthusiastic and passionate web developer based in India with more than half a decade of experience dedicated to deliver top-notch solutions and facilitate project success",
+    sameAs: [
+      miscellaneous
+        ? miscellaneous.facebookUrl
+        : "#",
+      miscellaneous ? miscellaneous.twitterUrl : "#",
+      miscellaneous
+        ? miscellaneous.linkedinUrl
+        : "#",
+      miscellaneous ? miscellaneous.githubUrl : "#",
+      miscellaneous
+        ? miscellaneous.instagramUrl
+        : "#"
+    ]
+  }
+
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      
       <div className="sticky z-30 top-5 w-full flex justify-end px-5">
         <ModeToggle />
       </div>
